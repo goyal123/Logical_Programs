7642717d58994eca6976f2f67031adbbef45ab68
goyal123 <akhilg642@gmail.com>
2022-07-19 13:39:03 +0530

    Adding code of Prime Number

---
+++ PrimeNumber.cs
@@ -0,0 +1,25 @@
+﻿namespace Logical_Programs
+{
+    public class PrimeNumber
+    {
+        public static void primenum(int num)
+        {
+            int flag = 0;
+            for (int i=2;i<= num/2; i++)
+            {
+                if (num % i == 0)
+                {
+                    flag= 1;
+                    break;
+                }
+            }
+            if (flag==0)
+                Console.WriteLine("Prime Number");
+            else
+                Console.WriteLine("Not a Prime Number");
+
+         
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@
             Console.WriteLine("Choose Given options for respective Programs to Run");
             Console.WriteLine("1 - Generate Fibonaaci Series");
             Console.WriteLine("2 - Check Perfect Number");
+            Console.WriteLine("3 - Check Perfect Number");
             options = Convert.ToInt32(Console.ReadLine());
             switch (options)
             {
@@ -24,6 +25,14 @@
                     num=Convert.ToInt32(Console.ReadLine());   
                     PerfectNumber.perfectnum(num);
                     break;
+
+                case 3:
+                    Console.WriteLine("Enter Number to check");
+                    num = Convert.ToInt32(Console.ReadLine());
+                    PrimeNumber.primenum(num);
+                    break;
+
+
 
 
             }
