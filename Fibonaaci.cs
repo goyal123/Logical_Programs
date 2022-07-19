c45e55b704e7f939be6fe4f863f2cc607b7a46c1
goyal123 <akhilg642@gmail.com>
2022-07-19 13:12:37 +0530

    Code of Fibonaaci

---
+++ Fibonaaci.cs
@@ -0,0 +1,25 @@
+﻿namespace Logical_Programs
+{
+    public class Fibonaaci
+    {
+        public static void fib(int num)
+        {
+            int first = 0, second = 1,next=0;
+            string ans = first + " " + second;
+            while(next<num)
+            {
+                next = first + second;
+                if (next < num)
+                    ans = ans + " " + next;
+                else
+                    break;
+                
+                first = second;
+                second = next;
+                //Console.WriteLine(next);
+            }
+            Console.WriteLine(ans);
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
+            int options,num;
+
             Console.WriteLine("Welcome to Logical Programs");
+            Console.WriteLine("Choose Given options for respective Programs to Run");
+            Console.WriteLine("1 - Fibonaaci Program");
+            options = Convert.ToInt32(Console.ReadLine());
+            switch (options)
+            {
+                case 1:
+                    Console.WriteLine("Enter Number to which Fibonaaci series to be printed");
+                    num = Convert.ToInt32(Console.ReadLine());
+                    Fibonaaci.fib(num);
+                    break;
+
+            }
         }
     }
 }
