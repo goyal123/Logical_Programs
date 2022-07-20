9ef552e6fbfec77eb0fdfe7c0d68303bdee24eeb
goyal123 <akhilg642@gmail.com>
2022-07-20 19:16:47 +0530

    Modified return type of function DectoBin

--- DecimaltoBinary.cs
+++ DecimaltoBinary.cs
@@ -2,17 +2,31 @@
 {
     public class DecimaltoBinary
     {
-        public static void DectoBin(int num)
+        public static string DectoBin(int num)
         {
-          string rem="";
-          while(num>1)
+            string rem = "";
+            
+            while (num > 1)
             {
                 int remainder = num % 2;
                 num = num / 2;
-                rem = Convert.ToString(remainder)+rem;
+                rem = Convert.ToString(remainder) + rem;
             }
-            rem=Convert.ToString(num)+rem;
-            Console.WriteLine("Binary Equivaent = "+rem);
+            rem = Convert.ToString(num) + rem;
+            if (rem.Length < 9)
+            {
+                var len = 8 - rem.Length;
+
+                for (int i = 0; i < len; i++)
+                {
+                    rem = '0' + rem;
+                }
+            }
+            return rem;
         }
     }
 }
+
+
+
+            
