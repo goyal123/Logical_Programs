2d25e910365a9fc2ecd03631ff7d81a6a248d51f
goyal123 <akhilg642@gmail.com>
2022-07-20 11:02:11 +0530

    Calculating elasped Time in every program using stopwatch

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Logical_Programs
+﻿using System.Diagnostics;
+
+namespace Logical_Programs
 {
     internal class Program
     {
@@ -14,44 +16,55 @@
             Console.WriteLine("4 - Generate Reverse of Number");
             Console.WriteLine("5 - Coupon check");
 
+            Stopwatch watch = new Stopwatch();
+
             options = Convert.ToInt32(Console.ReadLine());
+           
             switch (options)
             {
                 case 1:
+                    watch.Start();
                     Console.WriteLine("Enter Number to which Fibonaaci series to be printed");
                     num = Convert.ToInt32(Console.ReadLine());
                     Fibonaaci.fib(num);
+                    watch.Stop();
                     break;
 
                 case 2:
+                    watch.Start();
                     Console.WriteLine("Enter Number to check");
                     num=Convert.ToInt32(Console.ReadLine());   
                     PerfectNumber.perfectnum(num);
+                    watch.Stop();
                     break;
 
                 case 3:
+                    watch.Start();
                     Console.WriteLine("Enter Number to check");
                     num = Convert.ToInt32(Console.ReadLine());
                     PrimeNumber.primenum(num);
+                    watch.Stop();
                     break;
 
                 case 4:
+                    watch.Start();
                     Console.WriteLine("Enter Number to reverse");
                     num = Convert.ToInt32(Console.ReadLine());
                     Reverse_Number.Reverse(num);
+                    watch.Stop();
                     break;
 
                 case 5:
+                    watch.Start();
                     Console.WriteLine("Enter 2 digit Coupon Number");
                     num = Convert.ToInt32(Console.ReadLine());
                     Coupon.coupongen(num);
+                    watch.Stop();
                     break;
 
-
-
-
-
             }
+            
+            Console.WriteLine(watch.ElapsedMilliseconds);
         }
     }
 }
